3b32d4c7d3bd8bf4bfe16b90c3364a19ba9e4c3d
Michael Armesto <michaelarmestodev@gmail.com>
2024-05-26 22:20:03 -0300

    Agregando React Router

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
     > Fullstack Food Delivery Application using Reactjs
     >[Social](url)
 
+    - Tailwind CSS, React Router, React Icons, Framer Motion
+
     > ![Project Thumbnail]{url}
+
+npm
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import {Route, Routes} from "react-router-dom";
+import { Main, Login } from "./containers/index.ts";
+
 
 export const App: React.FC = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Hello, World!</h1>
+        <div className="w-screen min-h-screen h-auto flex flex-col items-center justify-center">
+            <Routes>
+                <Route path="/*" element={<Main />}/>
+                <Route path="/Login" element={<Login />}/>
+            </Routes>
         </div>
     );
 };
