9a3acea52283d2ca083309784e0d1d7538d6c899
MichaelArmesto <michaelarmestodev@gmail.com>
2024-07-06 23:55:00 -0300

    connecting Google auth to frontend

--- client/src/components/LoginInput.tsx
+++ client/src/components/LoginInput.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-
+import { motion } from 'framer-motion';
+import React, { useState } from 'react'
+import { fadeInOut } from '../Animations';
 
 interface LoginInputProps {
   placeholder: string;
@@ -11,8 +12,22 @@ interface LoginInputProps {
 }
 
 const LoginInput: React.FC<LoginInputProps> = ({placeholder,  icon, inputState, inputStateFn, type, isSignUp}) => {
+
+    const [isFocus, setIsFocus] = useState(false);
   return (
-    <div className={`flex items-center justify-center gap-4 bg-lightOverlay backdrop-blur-md rounded-md w-full px-4 py-2`}></div>
+    <motion.div {...fadeInOut}
+      className={`flex items-center justify-center gap-4 bg-lightOverlay backdrop-blur-md rounded-md w-full px-4 py-2 ${isFocus ? "shadow-md shadow-red-400" : "shadow-none"}`}>
+      {icon}
+      <input 
+        type={type} 
+        placeholder={placeholder} 
+        className='w-full h-full bg-transparent text-headingColor text-lg font-semibold border-none outline-none'
+        value={inputState}
+        onChange={(e) => inputStateFn(e.target.value)}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        />
+    </motion.div>
   )
 }
 
--- client/src/containers/Login.tsx
+++ client/src/containers/Login.tsx
@@ -1,11 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { LoginBg, Logo } from '../assets';
 import { LoginInput } from '../components';
+import { FaEnvelope, FaLock, FcGoogle } from "../assets/icons/index";
+import { motion } from "framer-motion";
+import { buttonClick } from '../Animations';
+
+import { getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import { app } from '../config/firebase.config';
 
 const Login: React.FC = () => {
 
   const [userEmail, setUserEmail] = useState("");
   const [isSignUp, setIsSignUp] = useState(false);
+  const [userPassword, setUserPaswword] = useState("");
+  const [confirm_password, setConfirm_password] = useState("");
+
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const logInWithGoogle = async () => {
+    await signInWithPopup(firebaseAuth, provider).then(userCred => {
+      firebaseAuth.onAuthStateChanged( cred => {
+        if( cred ){
+          cred.getIdToken().then( token => {
+            console.log(token);
+          })
+        }
+      })
+    })
+  }
+
   return (
     <div className='w-screen h-screen relative overflow-hidden flex'>
       {/*BG img*/}
@@ -14,7 +38,7 @@ const Login: React.FC = () => {
 
       {/* content box */}
 
-      <div className='flex flex-col items-center w-[20%] md:w-500 h-full z-10 bg-lightOverlay backdrop-blur-md p-4 px-4 py12 gap-6'>
+      <div className='flex flex-col items-center w-[25%] md:w-500 h-full z-10 bg-lightOverlay backdrop-blur-md p-4 px-4 py12 gap-6'>
           {/* Top Logo section */}
           <div className='flex items-center justify-start gap-4 w-full'>
             <img src={Logo} className='w-8' alt="" />
@@ -22,17 +46,70 @@ const Login: React.FC = () => {
           </div>
 
           <p className='text-3xl font-semibold text-headingColor'>Welcome!</p>
-          <p className='text-xl text-textColor -mt-6'>Sign in with following</p>
+          <p className='text-xl text-textColor -mt-6'>{isSignUp ? "Sign up" : "Sign in"} with following</p>
 
           {/* Inputs section */}
 
-          <div className='w-full flex flex-col items-center justify-center gap-6 px-4 md:px-12 py-4'>
-            <LoginInput placeholder='Email Here' icon="" inputState={userEmail} inputStateFn={setUserEmail} type='email' isSignUp={isSignUp}/>
-          </div>
-      </div>
+          <div className='w-full flex flex-col items-center justify-center gap-6 px-4 md:px-3 py-4'>
+            <LoginInput 
+              placeholder='Email Here' 
+              icon={<FaEnvelope className='text-xl text-textColor'/>} 
+              inputState={userEmail} 
+              inputStateFn={setUserEmail} 
+              type='email' 
+              isSignUp={isSignUp}
+            />
+            <LoginInput 
+              placeholder='Password' 
+              icon={<FaLock className='text-xl text-textColor'/>} 
+              inputState={userPassword} 
+              inputStateFn={setUserPaswword} 
+              type='password' 
+              isSignUp={isSignUp}
+            />
+            {isSignUp && (
+              <LoginInput 
+              placeholder='Confirm Password' 
+              icon={<FaLock className='text-xl text-textColor'/>} 
+              inputState={confirm_password} 
+              inputStateFn={setConfirm_password} 
+              type='password' 
+              isSignUp={isSignUp}
+            />
+            )}
 
+            {!isSignUp ? 
+              <p>Doesn't have an account? <motion.button {...buttonClick} className='text-red-400 underline cursor-pointer bg-transparent' onClick={() => setIsSignUp(true)}> Create One</motion.button></p>
+              :
+              <p>Already have an account? <motion.button {...buttonClick} className='text-red-400 underline cursor-pointer bg-transparent' onClick={() => setIsSignUp(false)}> Sign in</motion.button></p>
+            }
+
+            {isSignUp ? 
+              <motion.button {...buttonClick} className='w-full px-4 py-2 rounded-md bg-red-400 cursor-pointer text-white text-xl capitalize hover:bg-red-500 transition-all duration-150'>
+              Sign Up
+            </motion.button>
+            :
+            <motion.button {...buttonClick} className='w-full px-4 py-2 rounded-md bg-red-400 cursor-pointer text-white text-xl capitalize hover:bg-red-500 transition-all duration-150'>
+              Sign in
+            </motion.button> 
+          }
+          </div>
+
+          <div className='flex items-center justify-between gap-16'>
+            <div className='w-24 h-[1px] rounded-md bg-white'></div>
+            <p className='text-white'>or</p>
+            <div className='w-24 h-[1px] rounded-md bg-white'></div>
+          </div>
+
+          <motion.button {...buttonClick} 
+            className='flex items-center justify-center px-20 py-2 bg-lightOverlay backdrop-blur-md cursor-pointer rounded-3xl gap-4'
+            onClick={logInWithGoogle}
+          >
+            <FcGoogle className='text-3xl'/>
+            <p className='capitalize text-base text-headingColor'>Sign in with Google</p>
+          </motion.button>
+      </div>
     </div>
   )
 }
-
 export default Login;
